97df6c61691a48102a670ab15ec3c46af2e8f7f5
aitorgb <aitorguerrero9@gmail.com>
2020-08-23 18:55:56 +0200

    t2

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/ironcook';
+const MONGODB_URI = 'mongodb+srv://Aitor:Aitorgb9.@cluster0.p3vwi.mongodb.net/ironcook?retryWrites=true&w=majority'
+//process.env.MONGODB_URI || 'mongodb://localhost/ironcook';
 
 mongoose
   .connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
+    useCreateIndex: true
   })
   .then(() => console.log(`Successfully connected to the database ${MONGODB_URI}`))
   .catch((error) => {
