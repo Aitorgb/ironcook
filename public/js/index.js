d9b89476677e85ad617449210ab4d5c79ec70a21
aitorgb <aitorguerrero9@gmail.com>
2020-08-24 16:09:16 +0200

    ok

--- public/js/index.js
+++ public/js/index.js
@@ -4,18 +4,23 @@ const divIngredients = document.getElementById('allIngredients')
 const addSteps = document.getElementById('newSteps')
 const divSteps = document.getElementById('allSteps')
 
+if (addIngredient) {
+    addIngredient.addEventListener('click', () => {
+        const input = createdInput()
+        input.setAttribute('name', 'ingredients')
+        divIngredients.appendChild(input)
+    })
+} else if (addSteps) {
+    addSteps.addEventListener('click', () => {
+        const input = createdInput()
+        input.setAttribute('name', 'steps')
+        divSteps.appendChild(input)
+    })
+}
 
-addIngredient.addEventListener('click', () => {
-    const input = createdInput()
-    input.setAttribute('name', 'ingredients')
-    divIngredients.appendChild(input)
-})
 
-addSteps.addEventListener('click', () => {
-    const input = createdInput()
-    input.setAttribute('name', 'steps')
-    divSteps.appendChild(input)
-})
+
+
 
 function createdInput() {
     const input = document.createElement('input')
